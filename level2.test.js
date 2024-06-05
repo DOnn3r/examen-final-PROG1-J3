8282001163c4a8eba65a6d20e4786b8ec704997e
Sedera RAFALIMANANA <hei.sedera@gmail.com>
2024-06-05 10:01:19 +0300

    fix: level3.test.js

--- level2.test.js
+++ level2.test.js
@@ -6,33 +6,28 @@ import { reorganize } from "./level2.js";
 describe("Test with valid input", () => {
     describe("Test with combined upper and lower case letters", () => {
         it("Must group all occurrences of each letter, case insensitive", () =>
-            assert.strictEqual(reorganize("abdAbBce", "aAbbBdce"))
-        );
+            assert.strictEqual(reorganize("abdAbBce"), "aAbbBdce"))
     });
 
     describe("Test with all lowercase letters", () => {
         it("Must group all occurrences of each letter", () =>
-            assert.strictEqual(reorganize("aghigttepcpp", "agghittepppc"))
-        );
+            assert.strictEqual(reorganize("aghigttepcpp"), "agghittepppc"))
     });
-    
+
     describe("Test with all uppercase letters", () => {
         it("Must group all occurrences of each letter", () =>
-            assert.strictEqual(reorganize("SEDERARAKOTO", "SEEDRRAAKOOT"))
-        );
+            assert.strictEqual(reorganize("SEDERARAKOTO"), "SEEDRRAAKOOT"))
     });
 })
 
 describe("Test with invalid input", () => {
     describe("Test with an empty string", () => {
         it("Must return qn empty string", () =>
-            assert.strictEqual(reorganize("", ""))
-        );
+            assert.strictEqual(reorganize(""), ""))
     });
 
     describe("Test containing numbers or other special characters", () => {
         it("Should return a message error", () =>
-            assert.strictEqual(reorganize("mj23!$", "There are invalid string"))
-        );
+            assert.strictEqual(reorganize("mj23!$"), "There are invalid string"))
     });
 })
--- level3.test.js
+++ level3.test.js
@@ -3,8 +3,31 @@ import { it } from "mocha";
 import { assert } from "chai";
 import { getMaxVenue } from "./level3.js";
 
+describe("Test with valid input", () => {
+    describe("Test if there are people present at the same time", () => {
+        it("Must return the number of people present at the same time", () =>
+            assert.strictEqual(getMaxVenue(3, ["0 3600000", "1800000 7200000", "4800000 6000000"]), 2)
+        );
+    });
+
+    describe("Test if there are not people present at the same time", () => {
+        it("Must return 0", () =>
+            assert.strictEqual(getMaxVenue(3, ["0 1000000", "1100000 2000000", "2100000 3000000"]), 0)
+        );
+    });
+})
+
 describe("Test with invalid input", () => {
-    it("Should return a message error if input is invalid", () =>
-        assert.strictEqual(getMaxVenue())
-    );
-});
+    describe("Test if there are the same interval", () => {
+        it("Should return a message error", () =>
+            assert.strictEqual(getMaxVenue(3, ["0 3600000", "0 3600000", "4800000 6000000"]), "Interval must be different")
+        );
+    });
+
+    describe("Test if there are empty interval", () => {
+        it("Should return a message error", () =>
+            assert.strictEqual(getMaxVenue(3, ["0 1000000", "", "2100000 3000000"]), "There are an empty interval")
+        );
+    });
+})
+
